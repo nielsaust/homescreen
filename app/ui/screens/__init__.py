f774f6d1cd0a70da1bcb41aa9a6273d51374e071
Niels Aust <nielsaust@gmail.com>
2026-02-13 15:48:50 +0100

    phase8.0: introduce app.ui.screens namespace with compatible imports

---
+++ app/ui/screens/__init__.py
@@ -0,0 +1,11 @@
+from .menu_screen import MenuScreen
+from .music_screen import MusicScreen
+from .turned_off_screen import TurnedOffScreen
+from .weather_screen import WeatherScreen
+
+__all__ = [
+    "MenuScreen",
+    "MusicScreen",
+    "TurnedOffScreen",
+    "WeatherScreen",
+]
